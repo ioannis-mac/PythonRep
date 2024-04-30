6c0e421652d08e034d4afbe3ed63bec6924d0121
Ioannis M <Ioannis.Machaliotis@labom2iformation.fr>
2024-04-30 08:05:39 -0700

    Tests lance

--- Project2/aScript.py
+++ Project2/aScript.py
@@ -77,6 +77,7 @@ if __name__ == "__main__":
 
         elif systemNow == 'Windows':
             mis_a_jour_systemWINDOWS()
+            # Et les autres
 
         elif systemNow == "macOS":
             print('Il faut trouver les commandes')
--- Project2/test.py
+++ Project2/test.py
@@ -1,6 +1,11 @@
 import pytest
-from aScript import maintenanceLINUX
+from aScript import maintenanceLINUX, mis_a_jour_systemLINUX, verifier_integriteLINUX
+
+def mis_a_jour_systemLINUXShouldReturnErreur():
+    assert mis_a_jour_systemLINUX() == ""
 
 def maintenanceLINUXShouldReturn():
     assert maintenanceLINUX() == ""
 
+def verifier_integriteLINUXShouldReturnError():
+    assert verifier_integriteLINUX() == ""
